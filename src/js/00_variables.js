816248088234a4b786e63665346c3fd1db7404f1
Mariana Lobo <m.lobocasas@gmail.com>
2023-02-08 13:59:15 +0100

    add variables js

---
+++ src/js/00_variables.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log('>> Ready :)');
+
+const lyDesign = document.querySelector('.js-design');
+const arrowDesign = document.querySelector('.s-design-arrow');
+const lyFill = document.querySelector('.js-fill');
+const arrowFill = document.querySelector('.js-fill-arrow');
+const lyShare = document.querySelector('.js-share');
+
+
