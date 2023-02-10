e2ccd4528d3f642722a0ed86ec166efba6090e14
raquelgm88 <raquel.rgm7@gmail.com>
2023-02-10 13:21:45 +0100

    finished js collapsables

--- src/js/00_variables.js
+++ src/js/00_variables.js
@@ -3,12 +3,28 @@
 console.log('>> Ready :)');
 
 const designLeyend = document.querySelector('.js-design');
-const arrowDesign = document.querySelector('.s-design-arrow');
+const arrowDesign = document.querySelector('.js-design-arrow');
 const fillLeyend = document.querySelector('.js-fill');
 const arrowFill = document.querySelector('.js-fill-arrow');
 const shareLeyend = document.querySelector('.js-share');
-const designContent = document.querySelector('js-form1');
-const fillContent = document.querySelector('js-form2');
-const shareContent = document.querySelector('js-form3');
+const arrowShare = document.querySelector('.js-share-arrow');
+const designContent = document.querySelector('.js-form1');
+const fillContent = document.querySelector('.js-form2');
+const shareContent = document.querySelector('.js-form3');
+const inputName = document.querySelector('.js-input-name');
+const inputJob = document.querySelector('.js-input-job');
+const inputMail = document.querySelector('.js-input-mail');
+const inputPhone = document.querySelector('.js-input-phone');
+const inputLinkedin = document.querySelector('.js-input-linkedin');
+const inputGithub = document.querySelector('.js-input-github');
+const previewName = document.querySelector('.js-preview-name');
+const previewJob = document.querySelector('.js-preview-job');
+const previewPhone = document.querySelector('.js-preview-phone');
+const previewMail = document.querySelector('.js-preview-mail');
+const previewLinkedin = document.querySelector('.js-preview-linkedin');
+const previewGithub = document.querySelector('.js-preview-Github');
+
 
 //evento
+
+
--- src/js/01_colapsables.js
+++ src/js/01_colapsables.js
@@ -2,14 +2,58 @@
 
 console.log('>> Ready :)');
 //funciones
-function handleClickDesign(event) {}
+
+closeAllSections();
+openDesignSection();
+
 function closeAllSections() {
-  designContent.classlist.add('collapse');
+  designContent.classList.add('collapse');
   fillContent.classList.add('collapse');
   shareContent.classList.add('collapse');
+  arrowDesign.classList.remove('arrow-collapsed');
+  arrowFill.classList.remove('arrow-collapsed');
+  arrowShare.classList.remove('arrow-collapsed');
+
+  console.log('hola');
 }
-function openDesignSection() {}
+function openDesignSection() {
+  designContent.classList.remove('collapse');
+  
+}
+
+function handleClickDesign(event) {
+  closeAllSections();
+  openDesignSection();
+  arrowDesign.classList.add('arrow-collapsed');
+ 
+}
+
+function openFillSection() {
+  fillContent.classList.remove('collapse');
+  
+}
+
+function handleClickFill(event) {
+  closeAllSections();
+  openFillSection();
+  arrowFill.classList.add('arrow-collapsed');
+}
+
+function openShareSection() {
+  shareContent.classList.remove('collapse');
+  
+}
+
+function handleClickShare(event) {
+  closeAllSections();
+  openShareSection();
+  arrowShare.classList.add('arrow-collapsed');
+}
 
 //evento
 
 designLeyend.addEventListener('click', handleClickDesign);
+
+fillLeyend.addEventListener('click', handleClickFill);
+
+shareLeyend.addEventListener('click', handleClickShare);
