b4afbbda2c3c64b62becf05ead324f721df0e9de
Natalia Blanco <nataliaouteiral@gmail.com>
2023-02-10 13:48:20 +0100

    added js functions and events

--- src/js/03_preview.js
+++ src/js/03_preview.js
@@ -2,4 +2,21 @@
 
 console.log('>> Ready :)');
 
+function handleInputName(event) {
+  const nameWritten = inputName.value;
+  if (nameWritten === '') {
+    previewName.innerHTML = 'Nombre Apellidos';
+  } else {
+    previewName.innerHTML = nameWritten;
+  }
+  console.log('hola');
+}
 
+function handleInputMail(event) {
+  const mailWritten = inputMail.value;
+  previewMail.href = `mailto:${mailWritten}`;
+}
+console.log('hola');
+
+inputName.addEventListener('input', handleInputName);
+inputMail.addEventListener('input', handleInputMail);
