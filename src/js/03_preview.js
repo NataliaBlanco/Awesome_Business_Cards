7a6260cbb9cfd124142191b152502b18e48a45f1
Natalia Blanco <nataliaouteiral@gmail.com>
2023-02-16 13:53:12 +0100

    added cleaner functions

--- src/js/03_preview.js
+++ src/js/03_preview.js
@@ -2,43 +2,73 @@
 
 console.log('>> Ready :)');
 
-function handleInputName(event) {
+function updatePreview() {
+  if (data.name === '') {
+    previewName.innerHTML = 'Nombre Apellidos';
+  } else {
+    previewName.innerHTML = data.name;
+  }
+  previewMail.href = `mailto:${data.email}`;
+  if (data.job === '') {
+    previewJob.innerHTML = 'Front-end developer';
+  } else {
+    previewJob.innerHTML = data.job;
+  }
+  previewPhone.href = `tel:${data.phone}`;
+  previewLinkedin.href = `${data.linkedin}`;
+  previewGithub.href = `${data.github}`;
+}
+
+/* function handleInputName(event) {
   data.name = inputName.value;
   if (data.name === '') {
     previewName.innerHTML = 'Nombre Apellidos';
   } else {
     previewName.innerHTML = data.name;
   }
-  console.log('hola');
 }
-
-function handleInputMail(event) {
+ */
+/* function handleInputMail(event) {
   data.email = inputMail.value;
   previewMail.href = `mailto:${data.email}`;
 }
-console.log('hola');
+console.log('hola'); */
 
-function handleInputJob(event) {
+/* function handleInputJob(event) {
   data.job = inputJob.value;
   if (data.job === '') {
     previewJob.innerHTML = 'Front-end developer';
   } else {
     previewJob.innerHTML = data.job;
   }
-  console.log('hola');
-}
 
-function handleInputPhone(event) {
+} */
+
+/* function handleInputPhone(event) {
   data.phone = inputPhone.value;
   previewPhone.href = `tel:${data.phone}`;
 }
 function handleInputLinkedin(event) {
   data.Linkedin = inputLinkedin.value;
   previewLinkedin.href = `${data.Linkedin}`;
+} */
+
+function handleInputForm(event) {
+  data[event.target.name] = event.target.value;
+  updatePreview();
 }
 
-inputName.addEventListener('input', handleInputName);
+const allInputsList = document.querySelectorAll('.js_input');
+
+for (let i = 0; i < allInputsList.length; i++) {
+  allInputsList[i].addEventListener('input', handleInputForm);
+}
+
+/* allInputsList.addEventListener('input', handleInputForm); */
+
+/* inputName.addEventListener('input', handleInputName);
 inputMail.addEventListener('input', handleInputMail);
 inputJob.addEventListener('input', handleInputJob);
 inputPhone.addEventListener('input', handleInputPhone);
 inputLinkedin.addEventListener('input', handleInputLinkedin);
+ */
