58c92c219424404fa98ad466418c9681c9763972
Mariana Lobo <m.lobocasas@gmail.com>
2023-02-22 19:00:21 +0100

    improve share.js

--- src/js/05_share.js
+++ src/js/05_share.js
@@ -3,23 +3,23 @@
 
 function handleClickCreate(event) {
     event.preventDefault();
-
+    console.log(data)
     console.log('he hecho click')
-    fetch('https://dev.adalab.es/api/card/'),
+    fetch('https://dev.adalab.es/api/card/',
         {
             method: 'POST',
-            headers: {'content-type':'aplication/json'},
+            headers: {'content-type':'aplication/json',},
             body: JSON.stringify(data),
-        }
-        .then(response => response.json()) 
+        })
+        .then((response) => response.json()) 
         .then(data => {
-            if ( ! data.success) {
+            if ( data.success === false) {
                 console.log(data)
             } else {
                 console.log(data)
                 //direccion y twitter
            }
         });    
-};
+}
 
 btnCreate.addEventListener('click', handleClickCreate);
