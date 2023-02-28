aec8a81a4cf8891a357dc68637e6f3c51d95c2eb
Natalia Blanco <nataliaouteiral@gmail.com>
2023-02-28 14:15:29 +0100

    local storage data

--- src/js/05_share.js
+++ src/js/05_share.js
@@ -1,39 +1,45 @@
 'use strict';
+let urlCard = '';
 
 function renderSectionShare() {
-    
-    let html = `
+  let html = `
     <h2 class="js__h2">La tarjeta ha sido creada:</h2>
-    <a class="js__link_card" href="${data.cardURL}">${data.cardURL}</a>
+    <a class="js__link_card" href="${urlCard}">${urlCard}</a>
     <button class="js__twitter_button">
       <i class="fab fa-twitter"></i> Compartir en Twitter<a href=""></a>
     </button>
     <hr class="js__hr" />`;
-    cardResult.innerHTML = html;
+  cardResult.innerHTML = html;
 }
 
 function handleClickCreate(event) {
-    event.preventDefault();
-    console.log(data)
-    console.log('he hecho click')
-    fetch('https://dev.adalab.es/api/card/',
-        {
-            method: 'POST',
-            headers: {'content-type':'application/json',},
-            body: JSON.stringify(data),
-        })
-        .then((response) => response.json()) 
-        .then(data => {
-            if ( data.success === false) {
-                
-                urltext.innerHTML = 'Debes rellenar todos los campos'
-            } else {
-                console.log(data)
-                btnCreate.classList.add('hidden');
-                renderSectionShare();
-                
-           }
-        });    
+  event.preventDefault();
+  console.log(data);
+  console.log('he hecho click');
+  fetch('https://dev.adalab.es/api/card/', {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success === false) {
+        urltext.innerHTML = 'Debes rellenar todos los campos';
+      } else {
+        console.log(data);
+        btnCreate.classList.add('hidden');
+        urlCard = data.cardURL;
+        renderSectionShare();
+      }
+      localStorage.setItem('saveCard', JSON.stringify(data));
+    });
 }
+
+function renderLclStorage() {
+  const SvCard = JSON.parse(localStorage.getItem(saveCard));
+  renderSectionShare(SvCard);
+  updatePreview(SvCard);
+}
+renderLclStorage();
 
 btnCreate.addEventListener('click', handleClickCreate);
